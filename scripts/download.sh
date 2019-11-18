a98bedd1f89a4f5904a4f600d33235b551141a57
pablonavajas06 <pablonavajaschocarro6@gmail.com>
2019-11-18 19:01:25 +0100

    Add download.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,2 +1,11 @@
 # This script should download the file specified in the first argument ($1), place it in the directory specified in the second argument, 
 # and *optionally* uncompress the downloaded file with gunzip if the third argument contains the word "yes".
+    echo "Descargar genomas"
+    cd data
+    wget -i urls
+    echo
+    echo "Descargar contaminantes"
+    cd ..
+    wget -O res/contaminants.fasta.gz https://bioinformatics.cnio.es/data/courses/decont/contaminants.fasta.gz
+    gunzip -k res/contaminants.fasta.gz
+    echo

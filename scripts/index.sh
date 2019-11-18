5c17c2023436e3f36ecc07d038a30825c0b9b98a
pablonavajas06 <pablonavajaschocarro6@gmail.com>
2019-11-18 19:02:39 +0100

    Add index.sh

--- scripts/index.sh
+++ scripts/index.sh
@@ -1,6 +1,7 @@
 # This script should index the genome file specified in the first argument,
 # creating the index in a directory specified by the second argument.
-
 # The STAR command is provided for you. You should replace the parts surrounded by "<>" and uncomment it.
-
-# STAR --runThreadN 4 --runMode genomeGenerate --genomeDir <outdir> --genomeFastaFiles <genomefile> --genomeSAindexNbases 9
+    echo "Running STAR index"
+    mkdir -p res/contaminants_idx
+    STAR --runThreadN 4 --runMode genomeGenerate --genomeDir res/contaminants_idx/ --genomeFastaFiles res/contaminants.fasta --genomeSAindexNbases 9
+    echo

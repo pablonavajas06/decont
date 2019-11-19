0234db707bd4ebc7940e310bc2e651e2518f476b
pablonavajas06 <pablonavajaschocarro6@gmail.com>
2019-11-19 20:30:21 +0100

    Modificacion merge_script

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -1,3 +1,6 @@
 # This script should merge all files from a given sample (the sample id is provided in the third argument)
 # into a single file, which should be stored in the output directory specified by the second argument.
 # The directory containing the samples is indicated by the first argument.
+    echo "Merging compressed text files"
+    mkdir -p out/merged
+    cat $1/$3* $1/$3* > $2/$3.fastq.gz

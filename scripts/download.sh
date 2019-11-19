83105822a508c8249bddc2d843cf77cf44d5eefc
pablonavajas06 <pablonavajaschocarro6@gmail.com>
2019-11-19 09:00:10 +0100

    Add download.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,11 +1,14 @@
 # This script should download the file specified in the first argument ($1), place it in the directory specified in the second argument, 
 # and *optionally* uncompress the downloaded file with gunzip if the third argument contains the word "yes".
     echo "Descargar genomas"
-    cd data
-    wget -i urls
+    wget -P $2 $1
     echo
     echo "Descargar contaminantes"
-    cd ..
+if ($3=="yes")
+then
     wget -O res/contaminants.fasta.gz https://bioinformatics.cnio.es/data/courses/decont/contaminants.fasta.gz
     gunzip -k res/contaminants.fasta.gz
     echo
+else
+
+fi

866428d509796feec1b509e497cad42c462378ad
pablonavajas06 <pablonavajaschocarro6@gmail.com>
2019-11-20 20:31:24 +0100

    Nueva modificacion en el bonus_pipeline

--- scripts/bonus_pipeline.sh
+++ scripts/bonus_pipeline.sh
@@ -3,41 +3,56 @@ wget -i data/urls -P data/ -nc data/urls
 
 echo "Descaragar contaminantes"
 # Download the contaminants fasta file, and uncompress it
-bash scripts/download.sh https://bioinformatics.cnio.es/data/courses/decont/contaminants.fasta.gz res yes #$
+bash scripts/download.sh https://bioinformatics.cnio.es/data/courses/decont/contaminants.fasta.gz res yes #TODO
 
-echo "Index de los contaminantes"
+echo "Index contaminantes"
 # Index the contaminants file
 bash scripts/index.sh res/contaminants.fasta res/contaminants_idx
 
-echo "Merge de las muestras"
+echo "Merge muestras"
 # Merge the samples into a single file
 for sid in $(ls data/*.fastq.gz | cut -d "-" -f1 | sed 's:data/::') #TODO
 do
     bash scripts/merge_fastqs.sh data out/merged $sid
 done
 
-echo "Running cutadapt"
+echo "Running cutadpat"
 # TODO: run cutadapt for all merged files
     mkdir -p out/trimmed
     mkdir -p log/cutadapt
-for sid in $(ls out/merged/*.fastq.gz | cut -d "." -f1 | sed 's:out/merged/::')
+for sampleid in $(ls out/merged/*.fastq.gz | cut -d "." -f1 | sed 's:out/merged/::')
 do
-    cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed -o out/trimmed/${sid}.trimmed.fastq.gz out/$
-    echo "Todo va bien"
+    cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed -o out/trimmed/${sampleid}.trimmed.fastq.gz out/merged/${sampleid}.fastq.gz > log/cutadapt/${sampleid}.log
 done
 
-echo "Running STAR"
+echo "Running Star"
 #TODO: run STAR for all trimmed files
 for fname in out/trimmed/*.fastq.gz
 do
     # you will need to obtain the sample ID from the filename
-    sid=ls out/merged/*.fastq.gz | cut -d "." -f1 | sed 's:out/merged/::'
+    sid=$(echo $fname | sed 's:out/trimmed/::' | cut -d "." -f1)
     mkdir -p out/star/$sid
-    STAR --runThreadN 4 --genomeDir res/contaminants_idx/ --outReadsUnmapped Fastx --readFilesIn ${fname} -$
+    STAR --runThreadN 4 --genomeDir res/contaminants_idx --outReadsUnmapped Fastx --readFilesIn ${fname} --readFilesCommand zcat --outFileNamePrefix out/star/${sid}/
 done
 
 echo "Create Log_file"
-for sampleid in log/cutadapt/
+for sampleid in $(ls data/*.fastq.gz | cut -d "-" -f1 | sed 's:data/::' | sort | uniq)
 do
-    cat | grep "Reads with adapters and total basepairs" >> pipeline.log
+    echo "Sample: " $sampleid >> log/pipeline.log
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> log/pipeline.log
 
+    echo
+    echo
+    echo
+    echo
+    echo
+    echo
+    echo
+    echo
+    echo
+    echo
+done
+
+echo "Guardar Ambiente"
+mkdir -p envs
+conda env export > envs/decont.yaml

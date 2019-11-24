0419de5eedb6750738dd47a45de6015abfe3de08
pablonavajas06 <pablonavajaschocarro6@gmail.com>
2019-11-24 18:44:40 +0100

    Nueva modificacion bonus pipeline

--- scripts/bonus_pipeline.sh
+++ scripts/bonus_pipeline.sh
@@ -11,7 +11,7 @@ bash scripts/index.sh res/contaminants.fasta res/contaminants_idx
 
 echo "Merge muestras"
 # Merge the samples into a single file
-for sid in $(ls data/*.fastq.gz | cut -d "-" -f1 | sed 's:data/::') #TODO
+for sid in $(ls data/*.fastq.gz | cut -d "-" -f1 | sed 's:data/::' | sort | uniq) #TODO
 do
     bash scripts/merge_fastqs.sh data out/merged $sid
 done
@@ -20,19 +20,32 @@ echo "Running cutadpat"
 # TODO: run cutadapt for all merged files
     mkdir -p out/trimmed
     mkdir -p log/cutadapt
-for sampleid in $(ls out/merged/*.fastq.gz | cut -d "." -f1 | sed 's:out/merged/::')
+
+for sampleid in $(ls out/merged/*.fastq.gz | cut -d "." -f1 | sed 's:out/merged/::' | sort | uniq)
 do
-    cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed -o out/trimmed/${sampleid}.trimmed.fastq.gz out/merged/${sampleid}.fastq.gz > log/cutadapt/${sampleid}.log
+    if [ -f out/merged/${sampleid}-12.5dpp_sRNA_merged.fastq.gz ]
+    then
+        cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed -o out/trimmed/${sampleid}.trimmed.fastq.gz out/merged/${sampleid}.fastq.gz > log/cutadapt/${sampleid}.log
+    else
+        echo "ERROR: No se detectan los ficheros necesarios: $sampleid-12.5dpp_sRNA_merged.fastq.gz"
+        exit 1
+    fi
 done
 
 echo "Running Star"
 #TODO: run STAR for all trimmed files
 for fname in out/trimmed/*.fastq.gz
 do
-    # you will need to obtain the sample ID from the filename
-    sid=$(echo $fname | sed 's:out/trimmed/::' | cut -d "." -f1)
-    mkdir -p out/star/$sid
-    STAR --runThreadN 4 --genomeDir res/contaminants_idx --outReadsUnmapped Fastx --readFilesIn ${fname} --readFilesCommand zcat --outFileNamePrefix out/star/${sid}/
+    sid=$(echo ${fname} | sed 's:out/trimmed/::' | cut -d "." -f1)
+    mkdir -p out/star/${sid}
+
+    if [ -f ${fname} ]
+    then
+        STAR --runThreadN 4 --genomeDir res/contaminants_idx --outReadsUnmapped Fastx --readFilesIn ${fname} --readFilesCommand zcat --outFileNamePrefix out/star/${sid}/
+    else
+       echo "ERROR: No se detectan los ficheros necesarios: $fname or res/contaminants_idx"
+       exit 1
+    fi
 done
 
 echo "Create Log_file"

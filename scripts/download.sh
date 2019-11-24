af6a075b4bf10e75197d2bc9869e669478f2629a
pablonavajas06 <pablonavajaschocarro6@gmail.com>
2019-11-24 18:04:29 +0100

    nueva modificacion download.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,9 +1,8 @@
 # This script should download the file specified in the first argument ($1), place it in the directory specified in the second argument, 
 # and *optionally* uncompress the downloaded file with gunzip if the third argument contains the word "yes".
-    echo "Descargar genomas"
-    wget -P $2 $1
-    echo
-    echo "Descargar contaminantes"
+
+wget -P $2 $1
+
 if [ "$3" == "yes" ]
 then
     gunzip -k $2/$(basename $1)

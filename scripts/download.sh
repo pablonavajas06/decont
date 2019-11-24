23a51d384c9eaaa344a463d5c63c339227213e62
pablonavajas06 <pablonavajaschocarro6@gmail.com>
2019-11-24 20:35:07 +0100

    Nueva modificacion download

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,9 +1,9 @@
 # This script should download the file specified in the first argument ($1), place it in the directory specified in the second argument, 
 # and *optionally* uncompress the downloaded file with gunzip if the third argument contains the word "yes".
 
-wget -P $2 $1
+wget -P $2 $1 -nc
 
 if [ "$3" == "yes" ]
 then
-    gunzip -k $2/$(basename $1)
+    gunzip -fk $2/$(basename $1)
 fi
